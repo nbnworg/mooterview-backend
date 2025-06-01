6b1b31be51bae834cb445bc1cc1fda3f2f278779
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-06-01 11:01:54 +0530

    smithy modal integrated

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,22 +1,23 @@
 import { Router } from "express";
 import { signupUser } from "../services/users/signupUser";
 import { loginUser } from "../services/users/loginUser";
+import { CreateUserInput, CreateUserOutput } from "mooterview-server";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
   try {
-    const { username, email, password, fullName, location } = req.body;
-    if (!username || !email || !password || !fullName || !location) {
+    const input: CreateUserInput = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      fullName: req.body.fullname,
+      location: req.body.location,
+    };
+    if (!input) {
       res.status(400).json({ message: "Missing Required Fields" });
     }
-    const result = await signupUser(
-      username,
-      email,
-      password,
-      fullName,
-      location
-    );
+    const result: CreateUserOutput = await signupUser(input);
     res.status(200).json(result);
   } catch (error) {
     res.status(500).send(`Error login as admin: ${error}`);
--- src/services/users/signupUser.ts
+++ src/services/users/signupUser.ts
@@ -1,19 +1,22 @@
+import { CreateUserInput } from "mooterview-server";
 import { createCognitoUser } from "../../utils/commonCognitoMethods";
 import { putItemToDB } from "../../utils/commonDynamodbMethods";
+import { handleValidationErrors } from "../../utils/handleValidationError";
 
-export const signupUser = async (
-  username: string,
-  email: string,
-  password: string,
-  fullName: string,
-  location: string
-) => {
-  try {
-    const userId = await createCognitoUser(username, email, password);
-    await putItemToDB(userId, username, email, fullName, location);
-    return { message: "User signed up successfully!", userId };
-  } catch (error) {
-    console.error("Error occurred while sign up:", error);
-    throw error;
+export const signupUser = async (userInput: CreateUserInput) => {
+  const validateInput = CreateUserInput.validate(userInput);
+  handleValidationErrors(validateInput);
+
+  if (!userInput) {
+    throw new Error("Invalid Request, Missing Required Fields");
   }
+  const username = userInput.username!;
+  const email = userInput.email!;
+  const password = userInput.password!;
+  const fullName = userInput.fullName!;
+  const location = userInput.location!;
+
+  const userId = await createCognitoUser(username, email, password);
+  await putItemToDB(userId, username, email, fullName, location);
+  return { message: "User signed up successfully!", userId };
 };
