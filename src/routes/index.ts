cf88bf50e8fda564fb1135f12980e7522ab3a8f5
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-06-01 15:37:48 +0530

    problems route setup completed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import users from "./users.route";
+import problems from "./problems.route"
 
 const router = Router();
 
 router.use("/users", users);
+router.use("/problems", problems)
 
 export default router;
--- src/routes/problems.route.ts
+++ src/routes/problems.route.ts
@@ -2,5 +2,8 @@ import { Router } from "express";
 
 const router = Router();
 
+router.post("/", async (req, res) => {});
+router.get("/", async (req, res) => {});
+router.get("/:problemId", async (req, res) => {});
 
 export default router;
